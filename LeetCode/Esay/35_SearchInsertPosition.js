998f379724fbc41350cc8ce83c9fa8f3db7d6ae5
Sungwon Han <holyhansss@gmail.com>
2022-02-20 22:04:41 +0900

    add: add Binary problem done

---
+++ LeetCode/Esay/35_SearchInsertPosition.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+
+    let startIndex = 0;
+    let endIndex = nums.length-1;
+
+    
+
+
+
+};
