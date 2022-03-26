03d37432dd44ce24042338c6f545ee4d8e12ea61
Sungwon Han <holyhansss@gmail.com>
2022-03-26 12:48:46 +0900

    fix: 회전 문자열

--- INFLEARN_JS/section3/1_회전문자열.js
+++ INFLEARN_JS/section3/1_회전문자열.js
@@ -2,7 +2,7 @@
 function solution(word){
 
     word = word.toLowerCase();
-    for(let i=0;i<word.length/2; i++){
+    for(let i=0;i<Math.floor(word.length/2); i++){
         if(word[i] === word[word.length-1-i])
         continue;
         else return 'NO';
